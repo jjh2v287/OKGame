c5f05830475b9d865d550d8ff5cfea83761d55bb
jjh2v2 <jjh2v2@gmail.com>
2026-02-08 18:39:41 +0900

    AI용 기본 이동 컴포넌트 추가

---
+++ Source/OKGame/Components/OKCharacterMovementComponent.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OKCharacterMovementComponent.h"
+#include "CollisionDebugDrawingPublic.h"
+#include "NavigationSystem.h"
+#include "GameFramework/Pawn.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "NavMesh/RecastNavMesh.h"
+
+UOKCharacterMovementComponent::UOKCharacterMovementComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UOKCharacterMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	Mesh = PawnOwner->FindComponentByClass<USkeletalMeshComponent>();
+}
+
+
+void UOKCharacterMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(OKCharacterMovementComponent_Tick);
+	FOKScopedMeshBoneUpdateOverride ScopedNoMeshBoneUpdate(Mesh, EKinematicBonesUpdateToPhysics::SkipAllBones);
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
---
+++ Source/OKGame/Components/OKCharacterMovementComponent.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "OKCharacterMovementComponent.generated.h"
+
+struct FOKScopedMeshBoneUpdateOverride
+{
+	FOKScopedMeshBoneUpdateOverride(USkeletalMeshComponent* Mesh, EKinematicBonesUpdateToPhysics::Type OverrideSetting)
+		: MeshRef(Mesh)
+	{
+		if (MeshRef)
+		{
+			// 현재 상태 저장
+			SavedUpdateSetting = MeshRef->KinematicBonesUpdateType;
+			// 오버라이드 설정 적용
+			MeshRef->KinematicBonesUpdateType = OverrideSetting;
+		}
+	}
+
+	~FOKScopedMeshBoneUpdateOverride()
+	{
+		if (MeshRef)
+		{
+			// 원래 설정 복원
+			MeshRef->KinematicBonesUpdateType = SavedUpdateSetting;
+		}
+	}
+
+private:
+	USkeletalMeshComponent* MeshRef = nullptr;
+	EKinematicBonesUpdateToPhysics::Type SavedUpdateSetting;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class OKGAME_API UOKCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+public:
+	explicit UOKCharacterMovementComponent(const FObjectInitializer& ObjectInitializer);
+
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+	UPROPERTY(Transient, VisibleInstanceOnly, Category = "Move|Simple")
+	USkeletalMeshComponent* Mesh;
+};
